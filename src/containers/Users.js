3562acb7bce780cd9cf6515d2c6883da9487eff2
gabucino <gabriella@meggy.es>
2020-02-16 13:35:50 +0700

    initial commit

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome users like pizza</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
